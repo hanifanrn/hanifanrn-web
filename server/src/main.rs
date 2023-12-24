be86b5ec894edc35c7fee21a73b4a6705b8fe2a5
hanifanrn <hanifanrizki@gmail.com>
2023-12-24 22:54:54 +0700

    add swagger utoipa

--- server/src/main.rs
+++ server/src/main.rs
@@ -2,8 +2,34 @@ use axum::routing::get;
 use axum::{response::IntoResponse, Router};
 use tokio::net::TcpListener;
 
+use utoipa::OpenApi;
+use utoipa_rapidoc::RapiDoc;
+use utoipa_redoc::{Redoc, Servable};
+use utoipa_swagger_ui::SwaggerUi;
+
+#[derive(OpenApi)]
+#[openapi(
+        paths(
+        ),
+        components(
+            schemas()
+        ),
+        modifiers(),
+        tags(
+            (name = "Hanifanrn Website", description = "Hanifanrn website management API")
+        )
+    )]
+
+struct ApiDoc;
+
 pub fn routes_all() -> Router {
-    return Router::new().route("/", get(hello));
+    return Router::new()
+        .merge(SwaggerUi::new("/swagger-ui").url("/api-docs/openapi.json", ApiDoc::openapi()))
+        .merge(Redoc::with_url("/redoc", ApiDoc::openapi()))
+        // There is no need to create 'RapiDoc::with_openapi' because the OpenApi is served
+        // via SwaggerUi instead we only make rapidoc to point to the existing doc.
+        .merge(RapiDoc::new("/api-docs/openapi.json").path("/rapidoc"))
+        .route("/", get(hello));
 }
 
 pub async fn run() {
